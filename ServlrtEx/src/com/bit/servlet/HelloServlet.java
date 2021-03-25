d718b611fba8cc2b1312f2714c2750f4ffcea57a
gjtjdgh <gjtjdgh2@naver.com>
2021-03-25 11:50:28 +0900

    post 전송

--- ServlrtEx/src/com/bit/servlet/HelloServlet.java
+++ ServlrtEx/src/com/bit/servlet/HelloServlet.java
@@ -12,6 +12,25 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
 
 	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//post 방식 요청을 처리하기 위한 메서드
+		//폼으로 전송된 파라메터 받아오기
+		//입력 데이터 인코딩
+		req.setCharacterEncoding("UTF-8");
+		//	폼내의 input 태그 name 속성
+		String firstName = req.getParameter("last_name");
+		String lastName = req.getParameter("first_name");
+		
+		//응답 객체에서 Writer 얻어 온다
+		//응답되는 데이터가 text/html임을 브라우저에게 알려줌
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.println("<h1> From Data</h1>");
+		out.println("<p>성: "+firstName+"</p>");
+		out.println("<p>이름: "+lastName+"</p>");
+	}
+
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//get 요청을 처리하는 메서드 오버레이드
 		//클라이언트에서 name 파라메터를 받아서 환영 메서드 출력
