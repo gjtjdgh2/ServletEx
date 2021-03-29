d6e490383d505bb095e7e5df62cbef8565b20c32
gjtjdgh <gjtjdgh2@naver.com>
2021-03-29 14:22:22 +0900

    세션

--- ServlrtEx/WebContent/WEB-INF/views/includes/nav.jsp
+++ ServlrtEx/WebContent/WEB-INF/views/includes/nav.jsp
@@ -1,6 +1,20 @@
+<%@page import="com.bit.servlet.dao.UserVo"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
+    <%
+    UserVo authUser =(UserVo)session.getAttribute("authUser");
+    %>
+    
 <ul>
-<li><a href="<%= request.getContextPath() %>/users?a=loginform">로그인</a>
-		<li><a href="<%=request.getContextPath() %>/users?a=joinform">회원가입</a>
+<%
+	if(authUser ==null){
+%>
+<li><a href="<%= request.getContextPath() %>/users?a=loginform">로그인</a></li>
+		<li><a href="<%=request.getContextPath() %>/users?a=joinform">회원가입</a></li>
+		<%}else{ 
+		%>
+		<li><%=authUser.getName() %>님 환영합니다</li>
+		<li><a href="<%= request.getContextPath() %>/users?a=logout">로그아웃</a>
+		<%} %>
 	</ul>
--- ServlrtEx/src/com/bit/servlet/controller/UserServlet.java
+++ ServlrtEx/src/com/bit/servlet/controller/UserServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.bit.servlet.dao.UserDao;
 import com.bit.servlet.dao.UserDaoOrcalmpl;
@@ -33,6 +34,12 @@ public class UserServlet extends HttpServlet {
 		else if("joinsuccess".equals(action)) {
 			RequestDispatcher rd= req.getRequestDispatcher("/WEB-INF/views/users/joinsuccess.jsp");
 			rd.forward(req,resp);
+		}else if("logout".equals(action)) {
+			//a=logout ->세션 무효화 -> / 로 리다이렉트
+			HttpSession session = req.getSession();
+			session.invalidate();//전체 속성 삭제
+			
+			resp.sendRedirect(req.getContextPath() + "/");
 		}
 		
 		else {
@@ -68,6 +75,28 @@ public class UserServlet extends HttpServlet {
 				RequestDispatcher rd= req.getRequestDispatcher("/WEB-INF/views/users/joinform.jsp");
 				rd.forward(req,resp);
 			}
+		}
+		else if("login".equals(action)){
+			UserDao dao = new UserDaoOrcalmpl();
+			
+			String email = req.getParameter("email");
+			String password =req.getParameter("password");
+			
+			UserVo vo = dao.getUserByEmailAndPassword(email, password);
+			
+			if(vo !=null) {
+				//사용자 찾음
+				HttpSession session =req.getSession(true);
+				//사용자 정보 추가
+				session.setAttribute("authUser", vo);
+				//홈페이지로 다이렉트
+				resp.sendRedirect(req.getContextPath()+"/");
+			}else {
+			//그런자 없음->로그인 폼으로
+				resp.sendRedirect(req.getContextPath()+"/users?a=loginform");
+				
+			}
+			
 		}else {
 			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 				
